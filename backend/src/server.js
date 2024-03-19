f3a4becbc31d2b72bf3ce773cfb255f79d6df3ff
nguyendangkin <nguyenchin0077@gmail.com>
2024-03-19 12:21:05 +0700

    create frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 const port = process.env.PORT;
 
+app.use(cors());
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
